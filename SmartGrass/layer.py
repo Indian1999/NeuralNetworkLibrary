5f9f85d8b5470dbabea638e06ed8483ac28cbf6e
Indian1999 <haszab99@gmail.com>
2024-06-05 18:10:08 +0200

    Initial commit

---
+++ SmartGrass/layer.py
@@ -0,0 +1,12 @@
+#Layer absztrakt ősosztály
+#Ha egy osztály absztrakt, akkor azt sosem példányosítjuk
+class Layer:
+    def __init__(self):
+        self.input = None
+        self.output = None
+
+    def forward_propagation(self, input):
+        raise NotImplementedError
+
+    def backward_propagation(self, output_error, learning_rate):
+        raise NotImplementedError
